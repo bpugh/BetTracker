665798beea65a809a0bea93d3b8fa906ca383102
Brandon Pugh <bp@brandonpugh.com>
2013-07-06 12:39:48 -0500

    Fixed nuget restore

---
+++ BetApp/Models/Bet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetApp.Models
+{
+    public class Bet
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int CreatorId { get; set; }
+        public int PartnerId { get; set; }
+        public string Description { get; set; }
+    }
+}
